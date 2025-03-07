03f58017cd510f17a82409cb9fd3d7aa8dd3f1a8
Nugrohocn <nugrohocn13.com>
2025-03-07 21:45:18 +0700

    feat(search) : added search feature

---
+++ src/app/populer/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PopulerPage = () => {
+  return (
+    <div>
+      <h1>Ppopuler Page</h1>
+    </div>
+  );
+};
+
+export default PopulerPage;
--- src/components/AnimeList/index.js
+++ src/components/AnimeList/index.js
@@ -1,18 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const AnimeList = ({ images, title, id }) => {
+const AnimeList = ({ api }) => {
   return (
-    <Link href={`/${id}`} className="cursor-pointer">
-      <Image
-        src={images}
-        width={350}
-        height={350}
-        alt=""
-        className="max-h-64 w-full object-cover"
-      />
-      <h1 className="font-bold p-4 md:text-xl text-sm">{title}</h1>
-    </Link>
+    <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
+      {api.data.map((anime) => {
+        return (
+          <Link href={`/${anime.mal_id}`} className="cursor-pointer">
+            <Image
+              src={anime.images.webp.image_url}
+              width={350}
+              height={350}
+              alt=""
+              className="max-h-64 w-full object-cover"
+            />
+            <h1 className="font-bold p-4 md:text-xl text-sm">{anime.title}</h1>
+          </Link>
+        );
+      })}
+    </div>
   );
 };
 
